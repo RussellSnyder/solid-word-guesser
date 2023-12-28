7bf9ef62f4bb243c99298d32229c95aebda99c5e
Russell Snyder <russell@rustybass.comr>
2023-12-28 01:22:19 +0100

    remove dead code

--- src/utils/location.utils.ts
+++
@@ -1,8 +0,0 @@
-import { LetterInfo } from "../types";
-
-export const areLocationsEqual = (
-  location1: LetterInfo,
-  location2: LetterInfo
-) => {
-  return location1[0] === location2[0] && location1[1] === location2[1];
-};
